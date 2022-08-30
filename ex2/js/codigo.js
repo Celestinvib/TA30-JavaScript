08b22d0ca3ca12642ebcc3d7df715e447ea9f897
Celestinvib <celestinvib@gmail.com>
2022-08-30 20:03:22 +0200

    Ex2 v1.0

---
+++ ex2/js/codigo.js
@@ -0,0 +1,7 @@
+/* When opening the page index.html in the browser an alert with the following
+ * message will appear
+ */
+let msg = `Hola Mundo!\nQué fácil es incluir 'comillas simples' \ny "comillas dobles"`;
+
+//You can also show caracters like this "hola \"comillas dobles\""
+alert(msg);
