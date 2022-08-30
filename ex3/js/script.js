070f6a19406c451802054dd4ce44344b72416a15
Celestinvib <celestinvib@gmail.com>
2022-08-30 20:03:43 +0200

    Ex3 v1.0

---
+++ ex3/js/script.js
@@ -0,0 +1,5 @@
+let meses = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre'];
+
+for (let i  in meses) {
+    console.log(meses[i]);
+}
