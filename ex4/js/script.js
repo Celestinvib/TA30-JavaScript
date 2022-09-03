cecc3ab3ac9c2be00c643eb0aaf7d4f43175bf75
Celestinvib <celestinvib@gmail.com>
2022-09-03 11:07:00 +0200

    Ex4 v1.1

--- ex4/js/script.js
+++ ex4/js/script.js
@@ -1,17 +1,23 @@
 var valores = [true, 5, false, "hola", "adios", 2];
 
-let res1 = valores[3] > valores[4] ?  valores[3] : valores[4];
+//Added due to the request get the values of certain types from the main array
+const strings = valores.filter(value => typeof value === "string");
+const booleans = valores.filter(value => typeof value === "boolean");
+const numbers = valores.filter(value => typeof value === "number");
+
+
+let res1 = strings[0] > strings[1] ?  strings[0] : strings[1];
 
 //1.
 console.log("El elemento de texto mayor es "+res1);
 
 //2.
-console.log("resultado true : (valores[0] != valores[2] ) = "+ (valores[0] != valores[2]));
-console.log("resultado false : (valores[0] == valores[2]) ="+ (valores[0] == valores[2]));
+console.log("resultado true = "+ (booleans[0] != booleans[1]));
+console.log("resultado false = "+ (booleans[0] == booleans[1]));
 
 //3.
-console.log(valores[1]+" + "+ valores[5] +" = " +(valores[1] + valores[5] ));
-console.log(valores[1]+" - "+ valores[5] +" = " +(valores[1] - valores[5] ));
-console.log(valores[1]+" * "+ valores[5] +" = " +(valores[1] * valores[5] ));
-console.log(valores[1]+" / "+ valores[5] +" = " +(valores[1] / valores[5] ));
-console.log(valores[1]+" % "+ valores[5] +" = " +(valores[1] % valores[5] ));
+console.log(numbers[0]+" + "+ numbers[1] +" = " +(numbers[0] + numbers[1] ));
+console.log(numbers[0]+" - "+ numbers[1] +" = " +(numbers[0] - numbers[1] ));
+console.log(numbers[0]+" * "+ numbers[1] +" = " +(numbers[0] * numbers[1] ));
+console.log(numbers[0]+" / "+ numbers[1] +" = " +(numbers[0] / numbers[1] ));
+console.log(numbers[0]+" % "+ numbers[1] +" = " +(numbers[0] % numbers[1] ));
